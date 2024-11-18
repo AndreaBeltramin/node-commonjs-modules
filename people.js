a70101ca1137ef8cff37ffb405d1a26b97f63abc
AndreaBeltramin <andreabeltra99@gmail.com>
2024-11-18 16:14:08 +0100

    funzione in people.js

--- people.js
+++ people.js
@@ -1,10 +1,14 @@
 const hobbies = require("./hobbies");
 const getFullName = require("./names");
 
+const fullName = getFullName("Mario", "Rossi");
+const hobbiesOfPerson = hobbies("mare", "cucina", "libri");
+
 const person = () => {
 	return {
-		name: getFullName("Mario", "Rossi"),
-		hobbies: hobbies("mare", "cucina", "libri"),
+		firstName: fullName.firstName,
+		lastName: fullName.lastName,
+		hobbies: hobbiesOfPerson.hobbies,
 	};
 };
 console.log(person());
